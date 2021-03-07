581715d5515ca50bfe110659b3adff3f2a14ec13
lixinghang12 <54225231+lixinghang12@users.noreply.github.com>
2021-03-07 15:34:34 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Emobided Semantic Scene Graph Generation</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Abstract
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Semantic scene graph provides an effective way for intelligent agents to better understand the environment and it has been extensively used in many robotic applications. However, existing work mainly focuses on generating the scene graph from
+          the sensory information collected from a pre-defined path. To obtain a comprehensive semantic scene graph efficiently, the environment should be exhaustively explored with a carefully
+          designed path. In this paper, we propose a new task of Embodied Semantic Scene Graph, which exploits the embodiment of the intelligent agent to autonomously generate a best path to explore
+          the environment for scene graph generation. To this end, a learning framework with the paradigms of imitation learning and reinforcement learning is proposed to help the agent generate proper actions to explore the environment and the scene graph is
+          incrementally constructed. The proposed method is evaluated on the AI2Thor dataset using both the quantitative and qualitative performance indexes. Additionally, we implement the proposed
+          method on a streaming video captioning task and promising experimental results are achieved.
         </p>
 
         <div className={styles.grid}>
